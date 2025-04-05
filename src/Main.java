8ab12e038657a8617f51136522e12fc781d59ecf
Syrine BEN HASSINE <syrine.ben_hassine@etu.uca.fr>
2025-04-05 12:24:04 +0200

    add Main

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+package src;
+
+
+/**
+ * @author Syrine-BEN HASSINE
+ * @description This class represents the main program that allows players to play 
+ * alternately on the board. 
+ */
+
+
+public class Main {
+    
+}
