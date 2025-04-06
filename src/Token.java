0428b0f34be924006298760bec91003985551db9
JB <j-baptiste.largeron@etu.uca.fr>
2025-04-06 17:54:59 +0200

    add comment for token class

--- src/Token.java
+++ src/Token.java
@@ -1,5 +1,9 @@
 package src;
-
+/**
+ * The class represents a token that you can put in the grid, 
+ * it is the Atom of the game
+ * @author Largeron Jean-Baptiste + Elnasory Karam
+ */
 public class Token 
 {
     private  final Color color;
