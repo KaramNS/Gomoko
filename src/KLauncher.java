52190b274904ca624c60571cab1d62d0deca911f
Karam <elnasorykaram@gmail.com>
2025-05-01 22:38:18 +0200

    Added the KLauncher.java , as an alternatvie to Launcher.java , using a different approache and implementing the menuing, TO BE CONTINUED

---
+++ src/KLauncher.java
@@ -0,0 +1,113 @@
+package src;
+
+import java.io.IOException;
+
+/**
+ * KLauncher class representing the launcher for the game.
+ * It contains the main method to start the game.
+ * @author ELNASORY Karam
+ */
+
+public class KLauncher 
+{
+    private Game game ; // The on going game
+    private GameConditions gameConditions ; // The game conditions
+
+    /**
+     * 
+     */
+    public KLauncher () 
+    {
+        // Constructor for KLauncher class
+        this.game = null ;
+        this.gameConditions = new GameConditions() ; // Default game conditions
+    }
+
+    /**
+     * @description Main menu
+     */
+    public void mainMenu ()
+    {
+        System.out.println("Main Menu") ;
+        System.out.println("1. Start Game") ;
+        System.out.println("2. Load Game") ;
+        System.out.println("3. Settings (Setup a custome game conditions)") ; // IDEA
+        System.out.println("4. Exit") ;
+        System.out.print("Enter your choice: ") ;
+
+        int choice = Integer.parseInt(System.console().readLine()) ;
+
+        switch (choice) 
+        {
+            case 1:
+                this.game = new Game() ;
+                this.game.start() ;
+                break ;
+
+            case 2:
+                this.game = Game.loadGame() ;
+                this.game.start() ;
+                break ;
+
+            case 3:
+                System.out.println("Current game conditions:") ;
+                System.out.println(this.gameConditions.toString()) ;
+                System.out.println("Enter the new game conditions:") ;
+
+                this.gameConditions = GameConditions.customeConditions() ;
+                mainMenu();
+
+                break ;
+
+            case 4:
+                System.exit(0) ;
+                break ;
+
+            default:
+                System.out.println("Invalid choice") ;
+                mainMenu() ;
+                break ;
+        }
+    }
+
+    /** 
+     * @description Handles the inGame menu.
+     */
+    public void inGameMenu ()
+    {
+        System.out.println("In Game Menu") ;
+        System.out.println("1. Resume Game") ;
+        System.out.println("2. Save Game") ;
+        System.out.println("3. Exit without saving") ;
+        System.out.print("Enter your choice: ") ;
+
+        int choice = Integer.parseInt(System.console().readLine()) ;
+
+        switch (choice) 
+        {
+            case 1:
+                this.game.start() ;
+
+            case 2:
+                try 
+                {
+                    this.game.save() ;
+                } 
+                catch (IOException e) 
+                {
+                    e.printStackTrace();
+                }
+                break ;
+
+            case 3:
+                System.exit(0) ;
+                break ;
+
+            default:
+                System.out.println("Invalid choice") ;
+                inGameMenu() ;
+                break ;
+        }
+    }
+}
+
